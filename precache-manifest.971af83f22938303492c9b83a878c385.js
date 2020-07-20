75cad742188be495eb362ca4934a79e0bfaf7d19
Tarek Mostafa <tarek_sultana@yahoo.com>
2020-07-20 21:11:59 +0200

    Updates

---
+++ precache-manifest.971af83f22938303492c9b83a878c385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ebf7ef5df31221e9bac133df3ce1e8",
+    "url": "/Reactjs-Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "4c1734b136689a99fc47",
+    "url": "/Reactjs-Markdown-Previewer/static/css/main.e55b23e8.chunk.css"
+  },
+  {
+    "revision": "f6a7ac4f1225b03d6bd2",
+    "url": "/Reactjs-Markdown-Previewer/static/js/2.da83091e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactjs-Markdown-Previewer/static/js/2.da83091e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1734b136689a99fc47",
+    "url": "/Reactjs-Markdown-Previewer/static/js/main.1674f629.chunk.js"
+  },
+  {
+    "revision": "0b60747a7afdee6511bb",
+    "url": "/Reactjs-Markdown-Previewer/static/js/runtime-main.a5bcf193.js"
+  }
+]);
